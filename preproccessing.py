44ac8c8af02ecbcbf4e5ee52f6415a4b47868801
Dev Parikh <47342287+devparikh@users.noreply.github.com>
2022-02-01 22:06:56 -0500

    Update preproccessing.py

--- preproccessing.py
+++ preproccessing.py
@@ -5,16 +5,26 @@ import random
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import os
-import tensorflow as tf
+import keras
 import numpy as np
+import tensorflow_hub as hub
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+import tensorflow as tf
+from google.colab.patches import cv2_imshow
+import tensorflow_addons as tfa 
+from tensorflow.keras.layers import Embedding, LSTM, Dense, BatchNormalization
+from keras.applications.vgg16 import VGG16
 
 # Importing the images from Google Colab
 image_set = "/content/Images"
 image_dataset = []
 
-image_size = 299
+
+image_size = 224
 batch_size = 64
-epochs = 10
+epochs = 10 
 
 num_distinct_words = 5000
 embedding_dim = 10
@@ -30,38 +40,36 @@ for image in os.listdir(image_set):
   image = cv2.GaussianBlur(image, (5,5), 0, cv2.BORDER_DEFAULT)
 
   # we want to create this array where which the cv2 function on the next line will copy the original image and perform normalization
-  normalize_array = np.zeros((299,299))
+  normalize_array = np.zeros((224,224))
   image = cv2.normalize(image, normalize_array, 0, 255, cv2.NORM_MINMAX)
 
-  cv2.imshow(image)
+  #cv2.imshow(image)
 
   image_dataset.append(image)
 
 # Importing the captions from a CSV file from Google Colab
 caption_set = []
 
-image_captions = pd.read_csv("/content/Images/captions.txt")
+image_captions = pd.read_csv("/content/captions.txt")
 captions = image_captions["caption"]
 print(captions.head())
 
 for caption in captions:
   # Note that the since our captions are just used as a validation set by the model, we don't have to do any special preprocessing
-  # We have to execute 2 main steps:
-  # The first step is to convert our data from texts to sequences of numbers
-  # The second step is to limit our sequences to a certain length, to perform this we will have to truncate some sequences while padding others
-
-  # Define that Tokenizer that we are going to use to convert the texts to sequences of numbers
-  tokenizer = Tokenizer(num_words = 50, char_level=False, oov_token="UNK")
-  # Take the caption and fit it on this tokenizer
-  tokenizer.fit_on_texts(caption)
-
-  # Converting the text into sequences of numbers
-  caption = tokenizer.texts_to_sequences(caption)
-  # padding the inputs to make sure that they are all the same length when inputted into the model
-  padded_captions = tf.keras.preprocessing.sequence.pad_sequences(caption, maxlen=50, dtype="int32", padding="post", truncating="post", value=0)
-
   # After preparing our validation captions for the training of the model, we will append all of the sequences of captions to caption_set
   caption_set.append(caption)
+
+# We have to execute 2 main steps:
+# The first step is to convert our data from texts to sequences of numbers
+# The second step is to limit our sequences to a certain length, to perform this we will have to truncate some sequences while padding others
+tokenizer = Tokenizer(num_words=num_distinct_words, char_level=False, oov_token="UNK")
+# Take the caption and fit it on this tokenizer
+tokenizer.fit_on_texts(caption_set)
+
+# Converting the text into sequences of numbers
+caption_set = tokenizer.texts_to_sequences(caption_set)
+# padding the inputs to make sure that they are all the same length when inputted into the model
+caption_set = tf.keras.preprocessing.sequence.pad_sequences(caption_set, maxlen=40, dtype="int32", padding="post", truncating="post", value=0)
 
 # Zipping together the 2 datasets 
 image_captioning = list(zip(image_dataset, caption_set))
